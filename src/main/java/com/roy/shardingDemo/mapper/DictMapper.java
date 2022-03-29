c1ecb17ced36980a2dc840d59c41c1cc4deab12b
gaox <1103207268@qq.com>
2022-03-29 17:21:58 +0800

    备注

---
+++ src/main/java/com/roy/shardingDemo/mapper/DictMapper.java
@@ -0,0 +1,12 @@
+package com.roy.shardingDemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.roy.shardingDemo.entity.Dict;
+
+/**
+ * @author ：gaox
+ * @date ：Created in 2021/1/5
+ * @description:
+ **/
+public interface DictMapper extends BaseMapper<Dict> {
+}
